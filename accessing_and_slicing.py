b1ed8b3335aa2fd0661ba9347b9312abdc5df779
Sujan Adhikari <sujan69834@gmail.com>
2026-02-26 21:12:01 +0545

    Numpy 3

---
+++ accessing_and_slicing.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50, 60])
+
+print(arr[4])
+print(arr[1:4])
+print(arr[:4])
+print(arr[1:])
+print(arr[1:len(arr)])
+print(arr[::-1])
+print(arr[::2])
+print(arr[::])
